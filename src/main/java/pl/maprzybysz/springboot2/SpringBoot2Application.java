acc72f7061e36c4428c40f824a279c3d9e25a680
Mateusz Przybysz <notentego678@gmaill.com>
2021-04-27 12:06:15 +0200

    Spring Boot with Liquibase

---
+++ src/main/java/pl/maprzybysz/springboot2/SpringBoot2Application.java
@@ -0,0 +1,17 @@
+package pl.maprzybysz.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Application.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/pl/maprzybysz/springboot2/SpringBoot2ApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.maprzybysz.springboot2;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
